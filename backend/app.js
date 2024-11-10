12dbd0eae8f925c950b3677425bc8c28a88c2732
Shashwat Singh <shashwat123student@gmail.com>
2024-11-10 09:56:49 +0530

    Added Cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import connectDB from "./db/connectDB.js";
 import { v2 as cloudinary } from "cloudinary";
 import userRouter from "./Routes/Route.router.js";
@@ -14,17 +15,21 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET,
 });
 
-
 const app = express();
+
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
+
 app.use(cookieParser());
-app.use(express.json({ limit: "50mb" }));  
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/user", userRouter);
 
-app.use("/api/user",userRouter);
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("Server started on port " + PORT);
-})
+});
